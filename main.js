132cf4978d59e6aa73fa5d8ed349377093ba7fed
ObjectiveCFido <bselby2001@gmail.com>
2017-03-05 21:27:51 +0000

    Add email validation

--- main.js
+++ main.js
@@ -16,6 +16,20 @@ $(document).ready(function () {
 	
 		}
 		
+		if (email.indexOf('@') > -1) {
+			console.log("Good to go!");
+			if(document.getElementById("2").className == "has-error") {
+				document.getElementById("2").className -= "has-error";
+			}
+			
+			if(document.getElementById("2").className == "has-feedback") {
+				document.getElementById("2").className -= "has-feedback";
+			}
+		} else {
+			document.getElementById("2").className += " has-error";
+			document.getElementById("2").className += " has-feedback";
+		}
+		
 		if (msg == "") {
 			document.getElementById("3").className += " has-error";
 			document.getElementById("3").className += " has-feedback";			
