8eeb985a22117d3907c1b3cf9d8b0d59a751fc61
ObjectiveCFido <bselby2001@gmail.com>
2017-03-05 21:34:12 +0000

    Improve email validation with has-error/has-feedback class removal on correct email format

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ $(document).ready(function () {
 		
 		if (email.indexOf('@') > -1) {
 			console.log("Good to go!");
-			if(document.getElementById("2").className == "has-error") {
-				document.getElementById("2").className -= "has-error";
+			if(document.getElementById("2").classList.contains("has-error")) {
+				document.getElementById("2").classList.remove("has-error");
 			}
 			
-			if(document.getElementById("2").className == "has-feedback") {
-				document.getElementById("2").className -= "has-feedback";
+			if(document.getElementById("2").classList.contains("has-feedback")) {
+				document.getElementById("2").classList.remove("has-feedback");
 			}
 		} else {
 			document.getElementById("2").className += " has-error";
